9ce2907f7144050ab3a76af9c95d1b3989e3720e
samita100 <66769151+samita100@users.noreply.github.com>
2021-01-04 07:28:38 +0545

    Update check.py

--- check.py
+++ check.py
@@ -8,7 +8,7 @@ try:
   urllib.request.urlopen("https://onlineedlreg.dotm.gov.np/dlNewRegHome").getcode()
   #urllib.request.urlopen("https://google.com").getcode()
   
-  url = 'https://hooks.slack.com/services/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'
+  url = 'https://hooks.slack.com/services/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'
   myobj = {"text":"UP :)"}
   headers = {'content-type': 'application/json'}
 
@@ -19,7 +19,7 @@ try:
   
 except:
   print("Unable to open the website")
-  url1 = 'https://hooks.slack.com/services/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'
+  url1 = 'https://hooks.slack.com/services/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'
   myobj1 = {"text":"DOWN :("}
   headers1 = {'content-type': 'application/json'}
 
